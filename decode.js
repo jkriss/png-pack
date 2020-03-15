7be06c4ae6a78e1146bcc0ff5def765afce8061d
Jesse Kriss <jesse@jklabs.net>
2020-03-14 20:56:22 -0700

    Adding correct png header, making decoding work either way

--- decode.js
+++ decode.js
@@ -1,8 +1,9 @@
 const extract = require('png-chunks-extract')
 const { EMBEDDED_DATA_KEYWORD } = require('./common')
 
-const decode = (image) => {
-  const chunks = extract(image).filter(c => c.name === 'zEXt')
+const decode = (image, opts={}) => {
+  const chunks = extract(image).filter(c => c.name === 'zEXt' || c.name === 'zTXt')
+  const keyword = opts.keyword ? Buffer.from(opts.keyword) : EMBEDDED_DATA_KEYWORD
   for (let chunk of chunks) {
     let start = 0
     let nullByteCount = 0
@@ -11,7 +12,7 @@ const decode = (image) => {
       if (nullByteCount == 2) break
     }
     const keyword = Buffer.from(chunk.data.slice(0, start-1))
-    if (keyword.equals(EMBEDDED_DATA_KEYWORD)) {
+    if (keyword.equals(keyword)) {
       return Buffer.from(chunk.data.slice(start+1))
     }
   }
--- encode.js
+++ encode.js
@@ -2,12 +2,13 @@ const encodePng = require('png-chunks-encode')
 const extract = require('png-chunks-extract')
 const { EMBEDDED_DATA_KEYWORD } = require('./common')
 
-const encode = (image, data) => {
+const encode = (image, data, opts={}) => {
   const chunks = extract(image)
+  const keyword = opts.keyword ? Buffer.from(opts.keyword) : EMBEDDED_DATA_KEYWORD
   // Add new chunk before the IEND chunk
   chunks.splice(-1, 0, {
-    name: 'zEXt',
-    data: Buffer.concat([EMBEDDED_DATA_KEYWORD, Buffer.alloc(2), data])
+    name: 'zTXt',
+    data: Buffer.concat([keyword, Buffer.alloc(2), data])
   })
   return encodePng(chunks)
 }
